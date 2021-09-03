671819667b0a8ce0bd630a2600e6701aff869f9e
Chirag <sharma.chirag3737@gmail.com>
2021-09-03 19:52:19 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <View style={styles.container}>
+        <Text>Hi</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
